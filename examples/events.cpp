eabda08354e6229097e604d7bc08f789407f0f2f
ppetrov-dev <pppetrov92@gmail.com>
2020-05-17 21:09:47 +0300

    add examples.

---
+++ examples/events.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include "MyRotaryEncoder.h"
+
+#define PIN_EncoderClk 3
+#define PIN_EncoderDt 2
+#define PIN_EncoderSw 4
+
+MyRotaryEncoder _encoder = MyRotaryEncoder(PIN_EncoderClk, PIN_EncoderDt, PIN_EncoderSw);
+
+void OnClickHandler()
+{
+    Serial.println("Click");
+}
+void OnDoubleClickHandler()
+{
+    Serial.println("Double Click);
+}
+void OnLeftHoldTurnHandler()
+{
+    Serial.println("Left Hold Turn");
+}
+void OnLeftTurnHandler()
+{
+    Serial.println("Left Turn");
+}
+void OnRightHoldTurnHandler()
+{
+    Serial.println("Right Turn");
+}
+void OnPressStartHandler()
+{
+    Serial.println("Press Start");
+}
+void OnPressStopHandler()
+{
+    Serial.println("Press Stop");
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Serial OK");
+
+    _encoder.AttachOnClick(&OnClickHandler);
+    _encoder.AttachOnLeftHoldTurn(&OnLeftHoldTurnHandler);
+    _encoder.AttachOnRightHoldTurn(&OnRightHoldTurnHandler);
+    _encoder.AttachOnLeftTurn(&OnLeftTurnHandler);
+    _encoder.AttachOnRightTurn(&OnRightTurnHandler);
+    _encoder.AttachOnDoubleClick(&OnDoubleClickHandler);
+    _encoder.AttachOnPressStart(&OnPressStartHandler);
+    _encoder.AttachOnPressStop(&OnPressStopHandler);
+}
+
+void loop()
+{
+    _encoder.Tick();
+}
---
+++ examples/lambdaEvents.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include "MyRotaryEncoder.h"
+
+#define PIN_EncoderClk 3
+#define PIN_EncoderDt 2
+#define PIN_EncoderSw 4
+
+MyRotaryEncoder _encoder = MyRotaryEncoder(PIN_EncoderClk, PIN_EncoderDt, PIN_EncoderSw);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Serial OK");
+
+    _encoder.AttachOnClick([]() { Serial.println("Click"); });
+    _encoder.AttachOnLeftHoldTurn([]() { Serial.println("Left Hold Turn"); });
+    _encoder.AttachOnRightHoldTurn([]() { Serial.println("Right Hold Turn"); });
+    _encoder.AttachOnLeftTurn([]() { Serial.println("Left Turn"); });
+    _encoder.AttachOnRightTurn([]() { Serial.println("Right Turn"); });
+    _encoder.AttachOnDoubleClick([]() { Serial.println("Double Click"); });
+    _encoder.AttachOnPressStart([]() { Serial.println("Press Start"); });
+    _encoder.AttachOnPressStop([]() { Serial.println("Press Stop"); });
+}
+
+void loop()
+{
+    _encoder.Tick();
+}
